ac05f003a080759f719f984cb9f8dec8d4d8117a
Haeun Grace Jeong <grace.he.jeong@gmail.com>
2022-03-02 17:26:35 -0500

    Delete, Update Word APIs

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const wordsRouter = express.Router();
-const Word = require('../../db/models/word.model');
+const WordModel = require('../../db/models/word.model');
 
 /* 
 Get All Words List
 */
 wordsRouter.get('/', (req, res) => {
-	Word.find({}, (err, words) => {
+	WordModel.find({}, (err, words) => {
 		if (err) return console.error(err);
 		res.json({
 			words,
@@ -18,7 +18,7 @@ wordsRouter.get('/', (req, res) => {
 Add a new word
 */
 wordsRouter.post('/', (req, res) => {
-	const newWord = new Word(req.body);
+	const newWord = new WordModel(req.body);
 	newWord.save().then((savedWord) => {
 		res.json({
 			reply: savedWord,
@@ -31,8 +31,21 @@ wordsRouter.post('/', (req, res) => {
 Get a word by id
 */
 wordsRouter.get('/:id', (req, res) => {
-	res.json({
-		reply: 'get word by id success',
+	const wordId = req.params.id;
+	WordModel.findById(wordId, (err, word) => {
+		if (err) {
+			return console.log(err);
+		}
+		// // To do : check later
+		// if (!word) {
+		// 	return res.status(404).json({
+		// 		message: 'word not found',
+		// 	});
+		// }
+		res.json({
+			success: true,
+			word,
+		});
 	});
 });
 
@@ -40,9 +53,47 @@ wordsRouter.get('/:id', (req, res) => {
 Delete a word by id
 */
 wordsRouter.delete('/:id', (req, res) => {
-	res.json({
-		reply: 'delete word by id success',
+	const wordId = req.params.id;
+	WordModel.findByIdAndRemove(wordId, (err, deletedWord) => {
+		if (err) {
+			return console.log(err);
+		}
+		if (!deletedWord) {
+			return res.status(404).json({
+				message: 'word can not be deleted',
+			});
+		}
+		res.json({
+			reply: 'deleted word by id success',
+		});
 	});
+});
+
+/* 
+Update a word by id
+*/
+wordsRouter.put('/:id', (req, res) => {
+	const wordId = req.params.id;
+	const updatedBody = req.body;
+	WordModel.findByIdAndUpdate(
+		wordId,
+		updatedBody,
+		{ new: true },
+		(err, updatedWord) => {
+			if (err) {
+				return console.log(err);
+			}
+			// if (!updatedWord) {
+			// 	return res.status(404).json({
+			// 		message: 'word cannot be updated',
+			// 	});
+			// }
+			res.json({
+				reply: 'updated word by id success',
+				word: updatedWord,
+			});
+		}
+	);
 });
 
 module.exports = {
