cccae019df92d86284e2fc55c1c01c50e12e564d
Haeun Grace Jeong <grace.he.jeong@gmail.com>
2022-03-14 16:18:33 -0400

    connect with mongo cloud

--- db/index.js
+++ db/index.js
@@ -5,7 +5,6 @@ const { DATABASE_USER, DATABASE_PASSWORD, DATABASE_URL, DATABASE_NAME } =
 main().catch((err) => console.log(err));
 
 async function main() {
-	console.log(DATABASE_USER);
 	// await mongoose.connect('mongodb://localhost:27017/hello-word-db');
 	await mongoose.connect(
 		`mongodb+srv://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_URL}/${DATABASE_NAME}?retryWrites=true&w=majority`
